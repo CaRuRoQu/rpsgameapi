d54a481989cfe9792ff28822ca041cb55f5d6cab
Carlos Rodriguez Quella <130303815+CaRuRoQu@users.noreply.github.com>
2023-05-08 19:06:02 +0200

    Added api code
    Added api code

---
+++ src/main/java/local/rps_game_api/rpsgameapi/RpsgameapiApplication.java
@@ -0,0 +1,9 @@
+package local.rps_game_api.rpsgameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsgameapiApplication {
+	public static void main(String[] args) {SpringApplication.run(RpsgameapiApplication.class, args);}
+}
---
+++ src/test/java/local/rps_game_api/rpsgameapi/RpsgameapiApplicationTests.java
@@ -0,0 +1,13 @@
+package local.rps_game_api.rpsgameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsgameapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
